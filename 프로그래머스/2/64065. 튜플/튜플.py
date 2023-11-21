4be3ad9b1501eb308323912e6719266ff72d7746
Eunsu Kim <72302404+ag-su@users.noreply.github.com>
2023-11-21 12:45:45 +0900

    Update 튜플.py

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -2,7 +2,6 @@ def solution(s):
     answer = []
     lst_s = [list(map(int, s1.split(','))) for s1 in s[2:-2].split("},{")]
     
-    set1 = set()
     lst_s.sort(key=lambda x: len(x))
     
     set_prev = set()
