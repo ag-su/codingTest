7122b72048fd10562a35236370f0d8e163274b91
Eunsu Kim <72302404+ag-su@users.noreply.github.com>
2023-11-21 12:40:23 +0900

    [level 2] Title: 튜플, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = []
+    lst_s = [list(map(int, s1.split(','))) for s1 in s[2:-2].split("},{")]
+    
+    set1 = set()
+    lst_s.sort(key=lambda x: len(x))
+    
+    set_prev = set()
+    for i, nums in enumerate(lst_s):
+        set_nums = set(nums)
+        
+        
+        cur = set_nums - set_prev
+        answer.append(list(cur)[0])
+
+        set_prev = set_nums
+        
+    return answer
