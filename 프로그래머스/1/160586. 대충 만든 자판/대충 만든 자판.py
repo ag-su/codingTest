2dbf1351343703f103e36f529fbeaaeaeb12d4bb
Eunsu Kim <72302404+ag-su@users.noreply.github.com>
2023-12-12 16:00:29 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.81 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,20 @@
+def solution(keymap, targets):
+    dic_cnt = {}  
+    
+    for key in keymap: 
+        for i, k in enumerate(key): 
+            if (k in dic_cnt.keys()) and (i+1 >= dic_cnt[k]):
+                 continue
+            dic_cnt[k] = i+1 
+            
+    print(dic_cnt)
+    
+    answer = [0] * len(targets)
+    for i, target in enumerate(targets):
+        for t in target:
+            if t not in dic_cnt.keys():
+                answer[i] = -1 
+                break 
+            answer[i] += dic_cnt[t]
+    
+    return answer
