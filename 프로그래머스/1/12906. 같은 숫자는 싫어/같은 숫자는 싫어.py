d4d18d7308ce203d32dbe58960339ad724ab4391
Eunsu Kim <72302404+ag-su@users.noreply.github.com>
2024-04-09 16:18:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 65.44 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    
+    for a in arr:
+        if answer and answer[-1] == a: 
+            continue 
+        else:
+            answer.append(a) 
+    
+    return answer
