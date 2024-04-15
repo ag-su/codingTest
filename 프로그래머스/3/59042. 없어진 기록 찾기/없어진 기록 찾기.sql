59e965605cca510f9ca8138489c2dfbe037d3644
Eunsu Kim <72302404+ag-su@users.noreply.github.com>
2024-04-15 15:52:20 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+-- 입양을 간 기록은 있어 = ANIMAL_OUTS 테이블에 데이터 존재 
+-- 보호소에 들어온 기록은 없어 = ANIMAL_OUTS 테이블에 데이터 존재 AND ANIMAL_INS 테이블에 데이터 존재 안 한다. 
+
+-- 출력 컬럼: ANIMAL_ID, NAME
+-- 정렬: ANIMAL_ID ASC 
+
+SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_OUTS O 
+    WHERE NOT EXISTS (SELECT 1 
+                    FROM ANIMAL_INS I 
+                    WHERE O.ANIMAL_ID = I.ANIMAL_ID)
+    ORDER BY 1 
